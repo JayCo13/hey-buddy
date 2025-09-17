e3116b877e0a7b1ba13e8a84c1728f9118c0e788
JayCo13 <taicopgm@gmail.com>
2025-09-17 17:33:06 +0700

    update new frontend layout

---
+++ frontend/public/clear-cache.js
@@ -0,0 +1,20 @@
+// Clear all caches and service workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+      console.log('Service worker unregistered');
+    }
+  });
+}
+
+if ('caches' in window) {
+  caches.keys().then(function(names) {
+    for (let name of names) {
+      caches.delete(name);
+      console.log('Cache deleted:', name);
+    }
+  });
+}
+
+console.log('Cache clearing script executed');
